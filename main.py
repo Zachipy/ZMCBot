25ad5e7fa2c34ce44d724284c35e099740f7fbe2
Zachi <77886240+Zachipy@users.noreply.github.com>
2023-03-31 14:56:51 +0200

    ZMCBot Update 1.5
    The next big ZMCBot Update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Imports
 import disnake
 from disnake.ext import commands
 import urllib
@@ -5,50 +6,56 @@ import random
 import asyncio
 import openai
 
+# Lists
 answers = ['Yes', 'No', 'Maybe', 'Definitively', 'Could be', 'Absolutely!', 'Im sure about that', 'NO',
            'Nah', 'Probably', 'Im not sore about that', 'Very possible', 'I dont think so', 'Not possible']
 buchstabe = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
              'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
              'V', 'W', 'X', 'Y', 'Z']
 
+# Starting the Bot with desired Intents
+intents = disnake.Intents.default()
+intents.message_content=True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 insultsfile = open("assets/insults.txt")
 insults = insultsfile.read()
 oneinsult = insults.split("\n\n")
 insultsfile.close()
+
 tokenfile = open(".token")
 bottoken = tokenfile.read()
-print(f"Token:{bottoken}")
 tokenfile.close()
 
-bot = commands.Bot(command_prefix='!')
-
+openai_key = open(".openaitoken")
+bottoken = tokenfile.read()
+tokenfile.close()
 
 @bot.event
 async def on_ready():
     print('\r\nI hate it, but it works somehow')
-    await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name="In Space right now"),
+    await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name="Your preffered Status"),
                               status=disnake.Status.do_not_disturb)
 
 
-# Commands
+# Only Pictures Channel for example #setups Channel
+@bot.event
+async def on_message(message):
+    channel_id = 1089959374665039944 # Replace with your desired channel ID!
+    if message.channel.id == channel_id and len(message.attachments) == 0:
+        await message.delete()
+    elif message.channel.id == channel_id:
+        await message.add_reaction('👍')
+        await message.add_reaction('👎')
 
-
-#ChatGPT
-# Initialize the OpenAI API client
-openai.api_key = "api-key"
-
-# Create a Discord client object
+# GPT command
+openai.api_key = openai_key
 client =  disnake.Client()
-
-# Define the bot's behavior when it receives a message
 @bot.slash_command(description="Talk with ChatGPT")
 async def gpt(ctx, *, message):
     await ctx.response.defer()
-    # Only respond to messages that are not from the bot itself
     if ctx.author == client.user:
         return
-
-    # Use the OpenAI API to generate a response to the user's message
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt="Respond to: " + message,
@@ -57,24 +64,19 @@ async def gpt(ctx, *, message):
         stop=None,
         temperature=0.5,
     ).choices[0].text
-
-    # Send the generated response back to the user
     await ctx.edit_original_response(content=response)
 
 
-
-
-
-# Wetter CMD
+# Wheater command
 @bot.slash_command(description="Shows the weather")
-async def wetter(inter, *, wetter):
+async def wheater(inter, *, wetter):
     urllib.request.urlretrieve('http://wttr.in/{0}.png?0?q'.format(wetter), 'cache/wheather.png')
     await inter.send(f"Weather at {wetter}:", file=disnake.File('cache/wheather.png'))
 
 
-# Sagmir command
+# Tellme command
 @bot.slash_command(description="Helps you answer tricky questions!")
-async def sagmir(inter, *, question: str):
+async def tellme(inter, *, question: str):
     embed = disnake.Embed(color=0x0055ff,
                           description=f" {inter.author.mention} asked me:\r\n\r\n**{question}** \r\n\r\n My Answer is:\r\n\r\n **{random.choice(answers)}**")
     await inter.send(embed=embed)
@@ -88,7 +90,10 @@ async def delete(inter, amount):
       deleted = await inter.channel.purge(limit=count)
       await inter.response.send_message(f"Ich habe {len(deleted)} Nachrichten gelöscht", ephemeral=True)
 
-# Table
+
+
+# Soundboard commands
+# Soundboard: Table
 @bot.slash_command(description="Soundboard: oh noo, out table is broken")
 async def table(inter):
     channel = inter.author.voice.channel
@@ -100,8 +105,7 @@ async def table(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-
-# here we go again
+# Soundboard: Herewegoagain
 @bot.slash_command(description="Soundboard: Ah shit, here wo go again!")
 async def herewegoagain(inter):
     channel = inter.author.voice.channel
@@ -113,8 +117,7 @@ async def herewegoagain(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-# boom
-
+# Soundboard: Boom
 @bot.slash_command(description="Soundboard: Boom!")
 async def boom(inter):
     channel = inter.author.voice.channel
@@ -126,8 +129,7 @@ async def boom(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-# I AM NOT A MORON
-
+# Soundboard: Wheatley
 @bot.slash_command(description="Soundboard: Wheatley?")
 async def wheatley(inter):
     channel = inter.author.voice.channel
@@ -139,8 +141,7 @@ async def wheatley(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-# taco BEEREL FRLEGSIKGJIKOSRHJNORSNH
-
+# Soundboard: Tacobell
 @bot.slash_command(description="Soundboard: Bell")
 async def bell(inter):
     channel = inter.author.voice.channel
@@ -152,9 +153,7 @@ async def bell(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-
-# Speedrun
-
+# Soundboard: Speedrun
 @bot.slash_command(description="Soundboard: Speedrun")
 async def speedrun(inter):
     channel = inter.author.voice.channel
@@ -166,8 +165,7 @@ async def speedrun(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-# Russia moment
-
+# Soundboard: Russia
 @bot.slash_command(description="Soundboard: Russia")
 async def russia(inter):
     channel = inter.author.voice.channel
@@ -179,8 +177,7 @@ async def russia(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-# Fortnite
-
+# Soundboard: Fortnite
 @bot.slash_command(description="Soundboard: Fortnite")
 async def fortnite(inter):
     channel = inter.author.voice.channel
@@ -192,8 +189,7 @@ async def fortnite(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
-# Amogus
-
+# Soundboard: Amogus
 @bot.slash_command(description="Soundboard: Amogus")
 async def amogus(inter):
     channel = inter.author.voice.channel
@@ -205,11 +201,12 @@ async def amogus(inter):
     guild = inter.guild.voice_client
     await guild.disconnect()
 
+
+# Minecraft
 @bot.slash_command(description="Shows you the head of a Minecraft player")
-async def kopf(inter, username):
+async def head(inter, username):
     urllib.request.urlretrieve(f'https://cravatar.eu/helmhead/{username}/128.png', 'cache/Avatar.png')
     await inter.send(file=disnake.File('cache/Avatar.png'))
-
 
 @bot.slash_command(description="Show you the skin of a Minecraft player")
 async def skin(inter, username):
@@ -217,30 +214,25 @@ async def skin(inter, username):
     await inter.send(file=disnake.File('cache/skin.png'))
 
 
+# Mock
 @bot.slash_command(description="LeTs YoUr TExT lOok lIkE tHIs")
 async def mock(inter, *, text):
     def mock(input_text):
         output_text = ""
-
         for char in input_text:
-
             if char.isalpha():
-
                 if random.random() > 0.5:
                     output_text += char.upper()
-
                 else:
                     output_text += char.lower()
-
             else:
                 output_text += char
-
         return output_text
-
     mocktext = text
     await inter.send(mock(mocktext))
 
 
+# Numer guessing command
 @bot.slash_command(description="See if you can guess the number from the bot!")
 async def guess(inter, zahl: int):
     nummerbot = random.randint(0, 10)
@@ -260,30 +252,35 @@ async def guess(inter, zahl: int):
             await inter.send(embed=embed)
 
 
+# Media
+# Neurotoxien 
 @bot.slash_command(description="GlaDOS moment")
 async def glados(inter):
     await inter.send("credits:RaidGreg", file=disnake.File("video/glados.mp4"))
 
+# Wheatley moment
 @bot.slash_command(description="I AM NOT A MOROOOON")
 async def moron(inter):
     await inter.send("I AM  NOT A MORON!", file=disnake.File("audio/I am not a moron.mp3"))
 
+# Bentley
 @bot.slash_command(description="Bentley!")
 async def bentley(inter):
     await inter.send(file=disnake.File("assets/Bentley.png"))
 
+# Insult someone
 @bot.slash_command(description="Insult someone")
 async def insult(inter):
     await inter.send(random.choice(oneinsult))
 
+# Help command
 @bot.slash_command(description="Shows you the Help Menu")
 async def help(inter):
     embed = disnake.Embed(
-        title="Wheatley Help Menu",
-        description="A quick overview of all commands in Wheatley",
+        title="ZMC Help Menu",
+        description="A quick overview of all ZMCBot commands",
         color=disnake.Colour.blue(),
     )
-
     embed.set_author(
         name="ZMC",
         url="https://zachi.tk",
@@ -293,22 +290,17 @@ async def help(inter):
         text="WheatleyBot 1.2 (Zachi Fork) - Original developed by Oha Der Erste",
         icon_url="https://cdn.discordapp.com/avatars/913840049223774288/4a346a9cf7d80fc81806fe08459f5083.webp?size=128",
     )
-
-
-
-    embed.add_field(name="/wetter", value="Shows you the weather!", inline=False)
+    embed.add_field(name="/wheater", value="Shows you the weather!", inline=False)
     embed.add_field(name="/insult", value="Spits out a random insult!", inline=False)
-    embed.add_field(name="/sagmir {your yes-no-question}", value="Helps you make important decisions", inline=False)
-    embed.add_field(name="/table", value="Soundboard command. Try it while you are in a voice channel!", inline=False)
-    embed.add_field(name="/herewegoagain", value="Soundboard command. Try it while you are in a voice channel!", inline=False)
-    embed.add_field(name="/boom", value="Soundboard command. Try it while you are in a voice channel!", inline=False)
-    embed.add_field(name="/kopf {mc-name}", value="Shows you the head of a Minecraft Player", inline=False)
+    embed.add_field(name="/tellme {your yes-no-question}", value="Helps you make important decisions", inline=False)
+    embed.add_field(name="/head {mc-name}", value="Shows you the head of a Minecraft Player", inline=False)
     embed.add_field(name="/skin {mc-name", value="Shows you the skin of a Minecraft Player", inline=False)
     embed.add_field(name="/mock", value="MoCkS yOuR mEsSaGe", inline=False)
     embed.add_field(name="/guess {number}", value="Im thinking about a number and you have to guess it", inline=False)
     embed.add_field(name="/glados", value="Try it out LMAO", inline=False)
     embed.add_field(name="/moron", value="I AM NOT A MORON", inline=False)
     embed.add_field(name="/bentley", value="gives you a pick of the cutest cat ever!", inline=False)
+    embed.add_field(name="/gpt", value="Talk with GPT-3 (text-davinci-003)", inline=False)
     await inter.send(embed=embed)
 
 bot.run(bottoken)
